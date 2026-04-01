b665ae606a6fe09b36222c8b4404c2207de31341
Nicoly Leal <155275519+Nicleal@users.noreply.github.com>
2026-04-01 08:38:23 -0300

    Revise SQL queries for bdEscola database
    Updated SQL queries for the bdEscola database, including various SELECT statements with specific conditions.

--- Exercícios/BdEscola/SQLQuery_DQL.sql
+++ Exercícios/BdEscola/SQLQuery_DQL.sql
@@ -1,4 +1,29 @@
-use bd_ExConsultas
+use bdEscola
 
-Select nomeAluno 'Nome', rgAluno  'RG', dataNascimentoAluno  from bd_ExConsultas.dbo.tbAluno
-Where naturalidadeAluno like 'SP'
+SELECT nomeAluno 'Nome', rgAluno 'RG', dataNascimentoAluno FROM tb_Aluno 
+WHERE naturalidadeAluno LIKE 'sp'
+
+SELECT nomeAluno 'Nome', rgAluno 'RG' FROM tb_Aluno
+WHERE nomeAluno LIKE 'p%'
+
+SELECT nomeCurso FROM tb_Curso
+WHERE cargaHorariaCurso > 2000
+
+SELECT nomeAluno 'Nome', rgAluno 'RG' FROM tb_Aluno
+WHERE nomeAluno LIKE '%silva%'
+
+SELECT nomeAluno 'Nome', dataNascimentoAluno FROM tb_Aluno
+WHERE DATEPART(month,dataNascimentoAluno) = 3 
+
+SELECT codAluno 'Código', dataMatricula FROM tb_Matricula
+WHERE DATEPART(month,dataMatricula) = 5 
+
+SELECT codAluno 'Código' FROM tb_Matricula
+WHERE codTurma = 1 or codTurma = 2
+
+SELECT codALuno 'Código'  FROM tb_Matricula
+WHERE codTurma = 3
+
+SELECT * FROM tb_Aluno
+
+SELECT * FROM tb_Turma
